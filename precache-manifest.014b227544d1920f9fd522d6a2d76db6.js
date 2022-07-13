6115aa29f5e93eaa97991843d7986e523a0d9f0a
quinjoa7 <96493960+quinjoa7@users.noreply.github.com>
2022-07-13 18:46:10 -0300

    Updates

---
+++ precache-manifest.014b227544d1920f9fd522d6a2d76db6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d019b7c16f6d91aaa4e",
+    "url": "/Netflix-clone/static/css/main.2292a1d0.chunk.css"
+  },
+  {
+    "revision": "3d019b7c16f6d91aaa4e",
+    "url": "/Netflix-clone/static/js/main.3d019b7c.chunk.js"
+  },
+  {
+    "revision": "7984912c93713be75d82",
+    "url": "/Netflix-clone/static/js/1.7984912c.chunk.js"
+  },
+  {
+    "revision": "d74a3cbe7f1d210c39ef",
+    "url": "/Netflix-clone/static/css/2.517bd9da.chunk.css"
+  },
+  {
+    "revision": "d74a3cbe7f1d210c39ef",
+    "url": "/Netflix-clone/static/js/2.d74a3cbe.chunk.js"
+  },
+  {
+    "revision": "a67f4a16d67ef98fc487",
+    "url": "/Netflix-clone/static/js/runtime~main.a67f4a16.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Netflix-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Netflix-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Netflix-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Netflix-clone/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "abb0de80ec3875b931cdd7f2428761ad",
+    "url": "/Netflix-clone/index.html"
+  }
+];
